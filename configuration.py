183ce17c3f10e9dd2f4f99e8ffa7446fb3145269
SerhiiSkrypchenko <serhii.skrypchenko@firstbridge.io>
2023-02-14 12:41:12 +0200

    Update data

--- configuration.py
+++ configuration.py
@@ -6,4 +6,5 @@ prod_url = ""
 
 url = dev_url
 
-recovery_secret_phrase = "lake sock brief clown sadness into employ fatigue aware party index energy"
+recovery_secret_phrase = "attend uphold forest cover dismiss slot future live index wool issue grab"
+address = "0xf869a7cf9eb1703d9d716762271044f5b2b593da"
